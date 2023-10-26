5fffd2c258b43c4613be5ece3f55e5c6c2a505e6
Pingvu <nhlpool27@gmail.com>
2023-10-26 13:08:18 +0200

    Add project files.

---
+++ uppgift 3.4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Uppgift_3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många minuter är låten?");
+            int minuter = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många sekunder är låten?");
+            int sekunder = int.Parse(Console.ReadLine());
+
+            int totalaSekunder = minuter * 60 + sekunder;
+            int mingräns = 2 * 60 + 45;
+            int maxgräns = 4 * 60 + 20;
+
+            if (totalaSekunder >= mingräns && totalaSekunder <= maxgräns)
+            {
+                Console.WriteLine("Låten får spelas");
+                Console.ReadKey();
+
+            }
+            else
+            {
+                Console.WriteLine("Låten får nt spelas");
+                Console.ReadKey();
+            }
+        }
+    }
+}
